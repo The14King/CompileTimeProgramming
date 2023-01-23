91477723a31b19a4869d003f52e71531700ddb3d
Victor <523337@student.saxion.nl>
2023-01-23 19:24:55 +0100

    Start up

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+constexpr int value(int prevvalue){
+    return std::is_constant_evaluated() ? prevvalue * 2 : prevvalue * prevvalue;
+}
+constinit auto value1 = value(6);
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    auto value2 = value(6);
+    std::cout << value1 << '\n';
+    std::cout << value2 << '\n';
 }
