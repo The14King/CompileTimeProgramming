317c3f0783a8859e21d1234450ff9ef0b641bdb9
Victor <523337@student.saxion.nl>
2023-01-24 14:32:08 +0100

    Const code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <string>
 
-constexpr int value(int prevvalue){
-    return std::is_constant_evaluated() ? prevvalue * 2 : prevvalue * prevvalue;
-}
-constinit auto value1 = value(6);
+struct person{
+    person(std::string blood,std::string name,std::string gender):blood_type(std::move(blood)),name(std::move(name)),gender(std::move(gender)){}
+
+    void set_gender(const std::string& new_gender)const{
+        this->gender = new_gender;
+    }
+    const std::string blood_type;
+    const std::string name;
+    mutable std::string gender;
+};
 int main() {
-    auto value2 = value(6);
-    std::cout << value1 << '\n';
-    std::cout << value2 << '\n';
+    const auto victor = new person("A-", "victor", "male");
+
+    std::cout << victor->blood_type << '\t' << victor->name << '\t' << victor->gender << '\n';
+
+    //victor->name = "andrei";
+    victor->gender = "female";
+
+    std::cout << victor->blood_type << '\t' << victor->name << '\t' << victor->gender << '\n';
 }
