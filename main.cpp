d632c546deb355d466c89e70580fc574b3514449
Victor <523337@student.saxion.nl>
2023-01-24 15:21:33 +0100

    Constexpr code part 2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <string>
 
-// Const demo code
-struct person{
-    person(std::string blood,std::string name,std::string gender):blood_type(std::move(blood)),name(std::move(name)),gender(std::move(gender)){}
+//// Const demo code
+//struct person{
+//    person(std::string blood,std::string name,std::string gender):blood_type(std::move(blood)),name(std::move(name)),gender(std::move(gender)){}
+//
+//    void set_gender(const std::string& new_gender) const {this->gender = new_gender;}
+//    //void set_name(const std::string& new_name) const{this->name = new_name;}
+//
+//    const std::string blood_type;
+//    const std::string name;
+//    mutable std::string gender;
+//};
+//
+//// Constexpr variables demo code
+//constexpr int value = 10;
 
-    void set_gender(const std::string& new_gender) const {this->gender = new_gender;}
-    //void set_name(const std::string& new_name) const{this->name = new_name;}
-
-    const std::string blood_type;
-    const std::string name;
-    mutable std::string gender;
-};
-
-// Constexpr demo code
-constexpr int value1 = 10;
-
+// Constexpr functions demo code
+constexpr int DoubleIt(const int& number){
+    return number * 2;
+}
+constexpr int value = DoubleIt(5);
+int value2 = DoubleIt(5);
 int main() {
-    // Constexpr variables are implicit const
-    //value1 = 12;             // ERROR!
-    std::cout << value1 << '\n';
+    std::cout << value  << '\n';
+    std::cout << value2 << '\n';
 }
