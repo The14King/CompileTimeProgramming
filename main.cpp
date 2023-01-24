9dae89865e64a584e4bfbe44722dc29962e4a022
Victor <523337@student.saxion.nl>
2023-01-24 14:46:33 +0100

    Const code

--- main.cpp
+++ main.cpp
@@ -4,20 +4,19 @@
 struct person{
     person(std::string blood,std::string name,std::string gender):blood_type(std::move(blood)),name(std::move(name)),gender(std::move(gender)){}
 
-    void set_gender(const std::string& new_gender)const{
-        this->gender = new_gender;
-    }
+    void set_gender(const std::string& new_gender) const {this->gender = new_gender;}
+
     const std::string blood_type;
     const std::string name;
     mutable std::string gender;
 };
+
 int main() {
     const auto victor = new person("A-", "victor", "male");
+    // A-      victor  male
 
-    std::cout << victor->blood_type << '\t' << victor->name << '\t' << victor->gender << '\n';
+    victor->name = "andrei";            // ERROR!
+    victor->gender = "female";          // OK
 
-    //victor->name = "andrei";
-    victor->gender = "female";
-
-    std::cout << victor->blood_type << '\t' << victor->name << '\t' << victor->gender << '\n';
+    // A-      victor  female
 }
