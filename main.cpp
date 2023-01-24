d7dfbf111c69dff4b01f8bb806be5b516365be01
Victor <523337@student.saxion.nl>
2023-01-24 15:41:49 +0100

    Consteval code

--- main.cpp
+++ main.cpp
@@ -12,17 +12,31 @@
 //    const std::string name;
 //    mutable std::string gender;
 //};
-//
+
 //// Constexpr variables demo code
 //constexpr int value = 10;
 
-// Constexpr functions demo code
-constexpr int DoubleIt(const int& number){
+//// Constexpr functions demo code
+//constexpr int DoubleIt(const int& number){
+//    return number * 2;
+//}
+//constexpr int value = DoubleIt(5);
+//int value2 = DoubleIt(5);
+
+
+consteval int DoubleIt(const int& number){
     return number * 2;
 }
+// This is evaluated at compile time
 constexpr int value = DoubleIt(5);
+// This could be evaluated at compile time so it is
 int value2 = DoubleIt(5);
+
 int main() {
-    std::cout << value  << '\n';
+    std::cout << value << '\n';
     std::cout << value2 << '\n';
+
+    // This is a local variable => run time => ERROR!  DoubleIt() is a consteval function
+    int value3 = DoubleIt(value2);
+    std::cout << value3 << '\n';
 }
